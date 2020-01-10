371c87127df4556736f2a4113d650c240348216f
yelo <zhihuzeye@gmail.com>
2020-01-10 22:56:59 +0800

    feat: add form control

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classnames from 'classnames/bind'
 import Input from './Input'
 import Checkbox from './Checkbox'
@@ -7,21 +7,59 @@ import styles from './App.module.less'
 
 const cx = classnames.bind(styles)
 
+const isNumber = (n) => /^\d*$/.test(n)
+
 const App = () => {
+  const [ length, setLength ] = useState(256)
+  const [ checkboxs, setCheckboxs ] = useState({
+    uppercase: false,
+    lowercase: false,
+    numbers: false,
+    symbols: false,
+  })
+  const [ password, setPassword ] = useState('')
+
+  const submit = () => {
+    // TODO
+    setPassword('SOMELETTERS')
+  }
+
+  const onChangeLength = (e) => {
+    const value = e.target.value
+    if (!isNumber(value)) {
+      return
+    }
+    setLength(value)
+    submit()
+  }
+
+  const onClickCheckbox = (name) => () => {
+    setCheckboxs((checkboxs) => ({
+      ...checkboxs,
+      [name]: !checkboxs[name],
+    }))
+    submit() // TODO: double check
+  }
+
+  const onClickRegenerate = (e) => {
+    e.preventDefault()
+    submit()
+  }
+
   return <div className={cx('main')}>
     <div className={cx('header')}>Password Generator</div>
     <form className={cx('generator')}>
-      <Input label="Length" />
+      <Input label="Length" type="mobile" value={length} onChange={onChangeLength} />
       <div className={cx('checkboxs')}>
-        <Checkbox label="Uppercase" />
-        <Checkbox label="Lowercase" />
-        <Checkbox label="Numbers" />
-        <Checkbox label="Symbols" />
+        <Checkbox label="Uppercase" checked={checkboxs.uppercase} onClick={onClickCheckbox('uppercase')} />
+        <Checkbox label="Lowercase" checked={checkboxs.lowercase} onClick={onClickCheckbox('lowercase')} />
+        <Checkbox label="Numbers" checked={checkboxs.numbers} onClick={onClickCheckbox('numbers')} />
+        <Checkbox label="Symbols" checked={checkboxs.symbols} onClick={onClickCheckbox('symbols')} />
       </div>
-      <Button>Regenerate</Button>
+      <Button onClick={onClickRegenerate}>Regenerate</Button>
     </form>
     <div className={cx('result')}>
-      a#123asdf988a#123asdf988a#123asdf988a#123asdf988
+      {password}
     </div>
   </div>
 }
--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,9 +4,9 @@ import styles from './Checkbox.module.less'
 
 const cx = classnames.bind(styles)
 
-const Checkbox = ({ label, ...props }) => {
+const Checkbox = ({ label, onClick, ...props }) => {
   return (
-    <div className={cx('checkbox')}>
+    <div className={cx('checkbox')} onClick={onClick}>
       <input type="checkbox" {...props} />
       <span className={cx('checkmark')}></span>
       <label>{ label }</label>
